9c6a26e216b454d1c3a78b81c824b8bc70ffd2d4
Sandeep <itssharmasandeep@gmail.com>
2021-03-16 19:49:43 +0530

    Promises chaining

--- Promises/_02_promises.js
+++ Promises/_02_promises.js
@@ -8,8 +8,8 @@ let rejectPromise = new Promise((resolve, reject) => {
     setTimeout(() => reject(new Error("rejected")), 2000)
 })
 
-// a promise an only execute one resolve or reject statement. After this every statement will be ignored. It works like a "return"
-// in a standard function
+// a promise an only execute one resolve or reject statement. After this every statement will be ignored.
+// It works like a "return" in a standard function
 
 // then , catch and finally
 
@@ -19,3 +19,10 @@ rejectPromise.catch((error) => console.log(error))
 
 // finally runs after promise is settled whether it is resolved or rejected.
 // This can be used to stop the loading indicators and all
+
+
+// Promises chaining
+
+resolvePromise.then(() => console.log("chain 1"))
+               .then(() => console.log("chain 2"))
+               .then(() => console.log("chain 3"))
